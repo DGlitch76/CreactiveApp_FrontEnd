9627b83f3735f52c7886e3985b32cd8acee943d1
Diogo Guimaraes <mail@diogoguimaraes.pt>
2023-03-04 15:51:26 +0000

    history restore

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
+import LandingPage from './pages/LandingPage'
+
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
-import LandingPage from './pages/LandingPage'
 import ProfilePage from './pages/ProfilePage'
+
 import NewPhotoshootPage from './pages/NewPhotoshoot'
+import NewPhotoshootPage from './pages/NewPhotoshoot'
+
 import PhotoshootPage from './pages/PhotoshootPage'
 import PhotoshootApprovalPage from './pages/PhotoshootApproval'
 
 
 function App() {
+
+  const [allProjects, setProjects] = useState();
+
 
   return (
     <div className="App">
@@ -23,9 +30,12 @@ function App() {
 
         <Route path="/profile" element={<ProfilePage />} />
 
+        <Route path="/api/projects" element={<AllProjectsPage allProjects={allProjects} setProjects={setProjects}/>} />
+
+        <Route path="/api/project/:projectId" element={<PhotoshootPage />} />
+        <Route path="/api/approval/:prpojectId" element={<PhotoshootApprovalPage />} />
         <Route path="/newps" element={<NewPhotoshootPage />} />
-        <Route path="/:photographerId/photoshoot" element={<PhotoshootPage />} />
-        <Route path="/:clientId/approval" element={<PhotoshootApprovalPage />} />
+        <Route path="/newill" element={<NewIllustrationPage />} />
 
       </Routes>
 
