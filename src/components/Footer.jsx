c435626a364c85a966b5fb5bff30678085ee116f
Neagari <rodrigo.raimondi8@gmail.com>
2023-03-07 23:32:51 +0100

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@
 function Footer() {
   return (
     <div className="footer">
-      <img src={"https://punkt.de/_Resources/Persistent/7/5/b/d/75bd14d2ed44da6bf49616fd635a36fa4bb2eac8/React_Native_Logo-1196x628.png"} alt="reactLogo" style={{ height: '60px', width: '100px' }} />
+      <img src={"https://punkt.de/_Resources/Persistent/7/5/b/d/75bd14d2ed44da6bf49616fd635a36fa4bb2eac8/React_Native_Logo-1196x628.png"} alt="reactLogo" style={{ height: '60px', width: '110px' }} />
       <p>made with</p>
     </div>
   );
